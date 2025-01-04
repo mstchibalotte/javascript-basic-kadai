05636a6734749113b0f06f08ff97e7c17a516bc4
mstchibalotte <rikumst@gmail.com>
2025-01-04 11:13:07 +0900

    Update event.js

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -2,7 +2,6 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click' , () => {
-  //もしクリックされたらh2テキストを追加
   if (btn.click){
     setTimeout(() => {
       text.textContent = 'ボタンをクリックしました';
