0ff3830917a34b6389dcc51cb96ae334d3a2361f
mstchibalotte <rikumst@gmail.com>
2025-01-04 11:07:48 +0900

    kadai_021の再アップロード

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -5,14 +5,7 @@ btn.addEventListener('click' , () => {
   //もしクリックされたらh2テキストを追加
   if (btn.click){
     setTimeout(() => {
-      //新しくh2要素を作成し、定数に代入
-      const h2 =document.createElement('h2');
-      //作成したh2要素にテキストを新しく追加
-      h2.textContent = 'ボタンをクリックしました';
-      //HTML要素を追加
-      document.querySelector('h2').before(h2);
-      //既存のHTML要素の消去
-      text.style.display = 'none';
+      text.textContent = 'ボタンをクリックしました';
     } , 2000);
   }
 })
